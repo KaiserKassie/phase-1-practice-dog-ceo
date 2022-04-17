ee314a9bf4800dc56cbc83afeec939ede7f8a045
Kassie Becker <kassaundralee@hotmail.com>
2022-04-17 16:28:58 -0500

    commit tree a5d6e568a8eda2120fe0baa6674f7fc4ad7ba927

--- src/index.js
+++ src/index.js
@@ -1,1 +1,31 @@
-console.log('%c HI', 'color: firebrick')
+const url = 'https://dog.ceo/api/breeds'
+
+fetch (url + '/image/random/4') 
+.then((response) => response.json())
+.then((data) => renderDogs(data.message))
+
+function renderDogs(dogs) {
+    dogs.forEach(dog => {
+        const dogImage = document.createElement('img');
+        dogImage.src = dog;
+        const dogImageContainer = document.querySelector('#dog-image-container');
+        dogImageContainer.append(dogImage);
+    })
+}
+
+fetch (url + '/list/all')
+.then((response) => response.json())
+.then((data) => renderBreeds(data.message))
+
+function renderBreeds (breeds) {
+    for (const key in breeds) {
+        const breed = document.createElement('li');
+        breed.textContent = (`${key}: ${breeds[key]}`);
+        const breedContainer = document.querySelector('#dog-breeds');
+        breedContainer.append(breed);
+
+        breed.addEventListener('click' ,function(event){
+            event.target.style.color = 'red';
+        })
+    }
+}
